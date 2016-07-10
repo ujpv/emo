3f09bef3b0b273fed90e2bedb34c554c34af7c3e
Vladimir Kharitonov <ujpv@mail.ru>
2016-07-11 00:35:58 +0300

    2

--- mask2table.py
+++ mask2table.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import sys
 import progressbar
 
 MASK_FILENAME = "mask_granny_positions.txt"
@@ -9,10 +12,21 @@ MAX_DIST = 0.07
 mask = []
 head = []
 
-VERT_MASK_COUNT = 100500
+VERT_MASK_COUNT = 0
 
 def get_dist(p1, p2):
     return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2) ** (0.5)
+
+def get_min_max(model):
+    xs = [x for (x, _, _) in model]
+    ys = [y for (_, y, _) in model]
+    zs = [z for (_, _, z) in model]
+    msg = "X-Coordinate max {min_x}, max {max_x}\n" \
+          "Y-Coordinate max {min_y}, max {max_y}\n" \
+          "Z-Coordinate max {min_z}, max {max_z}".format(min_x=min(xs), max_x=max(xs),
+                                                         min_y=min(ys), max_y=max(ys),
+                                                         min_z=min(zs), max_z=max(zs))
+    return msg
 
 def get_nearest(p, model):
     min_dist = get_dist(p, model[0])
@@ -28,6 +42,18 @@ def get_nearest(p, model):
         return min_num
 
 
+###################################################################################
+
+if len(sys.argv) != 4:
+    print("Syntax error")
+    exit()
+else:
+    try:
+        MAX_DIST = float(sys.argv[3])
+    except BaseException:
+        print("Syntax error")
+        exit()
+
 for line in open(MASK_FILENAME, "r"):
     mask.append(tuple(map(float, line.split(' '))))
 
@@ -37,7 +63,9 @@ for line in open(HEAD_FILENAME, "r"):
 VERT_MASK_COUNT = len(mask)
 
 print("Mask loaded. Mask size: {}".format(len(mask)))
+print(get_min_max(mask))
 print("Head loaded. Head size: {}".format(len(head)))
+print(get_min_max(head))
 
 result = []
 bar = progressbar.ProgressBar(max_value=len(head),
@@ -45,6 +73,7 @@ bar = progressbar.ProgressBar(max_value=len(head),
                                   progressbar.Bar('=', '[', ']'),
                                   ' ',
                                   progressbar.Percentage()])
+
 for p in head:
     bar.update(bar.value + 1)
     result.append(get_nearest(p, mask))
@@ -53,4 +82,3 @@ with open(RESULT_FILE_NAME, mode='wt', encoding='utf-8') as myfile:
     myfile.write('\n'.join(str(x) for x in result))
 
 print("\nWriten {} numbers.".format(len(result)))
-
