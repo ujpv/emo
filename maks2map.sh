4b45c3bbaea89cc3eb55858f6a9406a0d32a0fb1
Vladimir Kharitonov <ujpv@mail.ru>
2016-08-05 00:36:11 +0300

    eyelashes for 30

--- maks2map.sh
+++ maks2map.sh
@@ -10,42 +10,9 @@
 ./mask2table.py vove/07_granny/head_default.txt   vove/07_granny/mask_fixed.txt   vove/07_granny/head_indexes.txt 0.1
 
 ./mask2table.py vove/00_boy_10/eyelashes.txt  vove/00_boy_10/mask_fixed.txt  vove/00_boy_10/eyelashes_indexes.txt 0.1
+./mask2table.py vove/02_man_30/eyelashes.txt  vove/02_man_30/mask_fixed.txt  vove/03_man_30/eyelashes_indexes.txt 0.1
 ./mask2table.py vove/01_boy_16/eyelashes.txt  vove/01_boy_16/mask_fixed.txt  vove/01_boy_16/eyelashes_indexes.txt 0.1
 ./mask2table.py vove/04_girl_10/eyelashes.txt vove/04_girl_10/mask_fixed.txt vove/04_girl_10/eyelashes_indexes.txt 0.1
+./mask2table.py vove/06_woman_30/eyelashes.txt vove/06_woman_30/mask_fixed.txt vove/06_woman_30/eyelashes_indexes.txt 0.1
 ./mask2table.py vove/07_granny/eyelashes.txt  vove/07_granny/mask_fixed.txt  vove/07_granny/eyelashes_indexes.txt 0.1
-
-#vove/00_boy_10/eyelashes.txt
-#vove/00_boy_10/eyelashes_indexes.txt
-#vove/00_boy_10/head_default.txt
-#vove/00_boy_10/head_indexes.txt
-#vove/00_boy_10/mask.txt
-#vove/00_boy_10/mask_fixed.txt
-#vove/01_boy_16/eyelashes.txt
-#vove/01_boy_16/head_default.txt
-#vove/01_boy_16/head_indexes.txt
-#vove/01_boy_16/mask.txt
-#vove/01_boy_16/mask_fixed.txt
-#vove/02_man_30/head_default.txt
-#vove/02_man_30/head_indexes.txt
-#vove/02_man_30/mask.txt
-#vove/02_man_30/mask_fixed.txt
-#vove/03_oldman/head_default.txt
-#vove/03_oldman/head_indexes.txt
-#vove/03_oldman/mask.txt
-#vove/03_oldman/mask_fixed.txt
-#vove/04_girl_10/eyelashes.txt
-#vove/04_girl_10/head_default.txt
-#vove/04_girl_10/mask.txt
-#vove/04_girl_10/mask_fixed.txt
-#vove/05_girl_16/head_default.txt
-#vove/05_girl_16/mask.txt
-#vove/05_girl_16/mask_fixed.txt
-#vove/06_woman_30/head_default.txt
-#vove/06_woman_30/mask.txt
-#vove/06_woman_30/mask_fixed.txt
-#vove/07_granny/eyelashes.txt
-#vove/07_granny/head_default.txt
-#vove/07_granny/mask.txt
-#vove/07_granny/mask_fixed.txt
-
 
